703230427e192976d3daf356dff9d6993d54fdb3
Kaustubh Bapat <kaustubh4694@gmail.com>
2018-08-23 21:02:15 -0400

    server code

---
+++ Server/sink.py
@@ -0,0 +1,127 @@
+import settings, threading, numpy, cv2, time, datetime, thread, signal, sys, os, zmq
+from recognition import Recognize
+from utils import RingBuffer
+from PIL import Image	
+
+class Config():
+	def __init__(self):
+		self.host_ip = sys.argv[1]
+		self.control_ip = settings.server['controller_ip']
+
+		#sets a limit on number of clients that can connect to a server
+		self.no_of_conn = int(settings.server['no_of_conn'])
+
+		#a structure consisting of identities of clients
+		self.client_list = []
+
+		#servID must be updated after JOIN request to controller
+		self.servID = "0" 
+
+		#client specific - may need to be pushed to another client specific class
+		self.ring_buffer = RingBuffer(int(settings.server['buffer_size']))
+		self.open_alpr = Recognize(int(settings.server['predictions']), "ca", os.getcwd())
+		self.complete = False   #may need revision
+		self.msg_count=0 		#only for debug purposes
+		
+		#Bind server to ports
+		context = zmq.Context()
+		#To send requests to controller
+		self.control_port = settings.server['control_port']
+		self.control = context.socket(zmq.REQ)
+		self.control.connect("tcp://"+self.control_ip+":"+self.control_port)
+
+		#To respond to requests of client(s)
+		self.command_port = settings.server['command_port']
+		self.command = context.socket(zmq.REP)
+		self.command.bind("tcp://"+self.host_ip+":"+self.command_port)
+
+		#To pull frames from client(s)
+		self.data_port = settings.server['data_port']
+		self.receiver = context.socket(zmq.PULL)
+		self.receiver.bind("tcp://"+self.host_ip+":"+self.data_port)
+
+class ReceiveFrames():
+	def _get_stream(self,config):
+		print("Ready to receive stream....")
+		while not config.complete:
+			#print(".")
+			self.current_frame_string = self.__utility(config, True)
+			self.np_array = numpy.fromstring(self.current_frame_string, numpy.uint8)
+			self.current_frame = cv2.imdecode(self.np_array, cv2.IMREAD_COLOR)
+			
+			if type(self.current_frame) is not type(None): 
+				self.__insert_in_ring_buffer(config, self.current_frame)
+
+		print("frames received = "+str(config.msg_count))
+		print("--------------------All frames received----------------------")
+
+	def __utility(self, config, run):
+		data = b''
+		while run:
+			try: 
+				string = config.receiver.recv()   						
+				if len(string) == 0:
+					print("Length of frame is zero")
+					break
+				
+				index = string.find('END!')
+				if index != -1:
+					data += string[:index]
+					config.msg_count = config.msg_count+1
+					#print(".") #just to see the progress
+					#print("END of frame received")
+					break	
+
+				index = string.find('STOP!')
+				if index != -1:
+					data += string[:index]
+					run = False
+					config.complete = True
+					print("STOP received")				
+				
+				data += string
+			except:
+				print("Exception: capture failed")
+				continue
+		return data
+	
+	def __insert_in_ring_buffer(self, config, frame):            
+		config.ring_buffer.lock.acquire()
+		fwd = config.ring_buffer.head + 1
+		if fwd == config.ring_buffer.length: fwd = 0
+		while fwd == config.ring_buffer.tail:
+			config.ring_buffer.lock.wait()
+			#print("I am full")
+
+		config.ring_buffer.push(config.ring_buffer.head, frame)
+		config.ring_buffer.head = fwd
+		#print("pushed to ring_buffer")
+		config.ring_buffer.lock.notify()
+		config.ring_buffer.lock.release()           
+
+class AlprProcessing():
+	def _put_alpr(self, config):
+		self.__remove_from_ring_buffer(config)
+		#print("in put_alpr")
+		print("-------------------End of Alpr processing------------------")
+
+	def __preprocess(self, frame): 
+		return frame
+
+	def __remove_from_ring_buffer(self, config):
+		while 1:
+			#print("Inside remove block")
+			config.ring_buffer.lock.acquire()
+			if config.ring_buffer.tail == config.ring_buffer.head and config.complete:
+				config.ring_buffer.lock.release()
+				break
+			while config.ring_buffer.tail == config.ring_buffer.head:
+				config.ring_buffer.lock.wait()
+
+			fwd = config.ring_buffer.tail + 1
+			if fwd == config.ring_buffer.length: fwd = 0
+			frame = config.ring_buffer.pull(config.ring_buffer.tail)
+			config.ring_buffer.tail = fwd
+			config.ring_buffer.lock.notify()
+			config.ring_buffer.lock.release()                    
+			config.open_alpr.put(self.__preprocess(frame))
---
+++ Server/utils.py
@@ -0,0 +1,91 @@
+import threading, thread
+
+class StoppableThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()        
+
+    def stop(self):
+    	print("Stop thread called")
+        if self.mythread.is_alive() == True:
+            # set event to signal thread to terminate
+            self.stop_event.set()
+            print("Server: Preparing to stop a thread, waiting on join")
+            # block calling thread until thread really has terminated
+            self.mythread.join()
+        else:
+        	print("Server: ERROR")
+
+#Spawns thread for scanning requests/commands from controller and responds to those
+class Scan(StoppableThread):
+	def __init__(self, config):
+		StoppableThread.__init__(self)
+		self.mythread = threading.Thread(name="Listen_controller_commands", target=self.run, args=(config,))
+		self.mythread.start()
+
+	def run(self, config):
+		#stop_event inherited from StoppableThread class
+		while self.stop_event.is_set()==False:
+			self._scan(config)
+
+	def _scan(self, config):
+		receive = config.command.recv()
+		print(receive)
+		#print("checking for "+"CHECK!"+config.host_ip+config.command_port)
+		if receive=="CHECK!"+config.host_ip+":"+config.command_port:
+			print("Controller: CHECK!")
+			config.command.send("200!")
+			print("Server: 200!")
+
+		#Connect clients to server
+		elif receive[:8] == "CONNECT!":
+			if len(config.client_list)<config.no_of_conn:
+				config.client_list.append(receive[8:])
+				reply = "200!"+config.data_port
+				#print("Sending reply as "+self.reply)
+				#200 - SUCCESS
+				config.command.send(reply)
+			else:
+				#503 - Service Unavailable, connection limit reached
+				print("Server: Connection limit reached")
+				config.command.send("503!") 
+
+		elif receive[:11] == "DISCONNECT!":
+			if receive[11:] in config.client_list:
+				config.client_list.remove(receive[11:])
+				print("Server: client "+receive[11:]+" disconnected")
+				config.command.send("200!")
+			else:
+				config.command.send("400!")
+		else:
+			#400 - Bad request
+			config.command.send("400!")
+			print("Server: 400!")
+
+class Control():
+	@staticmethod
+	def _join(config):
+		config.control.send_multipart(["JOIN!", config.servID, config.host_ip, config.command_port])
+		receive = config.control.recv()
+		print("Controller: "+receive)
+		if receive[:4]=="200!":
+			#print(receive)
+			config.servID = receive[4:]
+			return 0
+		else:
+			print("Server: ERROR")
+			return -1
+
+class RingBuffer():
+	def __init__(self, size):
+		self.buffer = [None]*size
+		self.lock = threading.Condition(threading.Lock())
+		self.head = 0
+		self.tail = 0
+		self.length = size
+
+	def push(self, index, frame):
+		self.buffer[index] = frame
+ 
+	def pull(self, index):
+		 return self.buffer[index]
